ccc71683f5aaf7be150506fdcec46571a62faa05
Chris <38708476+wchrisk@users.noreply.github.com>
2020-01-07 14:07:13 -0500

    Work on frontend

---
+++ frontend/src/components/Logs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Logs.css';
+
+class Logs extends React.Component {
+    render() {
+        return <div class="logs">
+            Logs
+        </div>
+    }
+}
+
+export default Logs;
---
+++ frontend/src/components/Voting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Voting.css';
+
+class Voting extends React.Component {
+    render() {
+        return <div class="voting">
+            Voting
+        </div>
+    }
+}
+
+export default Voting;
